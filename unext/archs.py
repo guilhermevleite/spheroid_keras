40868b486d4ce982656849acd524f004b8632fd6
Guilherme Leite <guilherme.vieira.leite@gmail.com>
2022-11-16 13:37:40 -0300

    unetr ready for testing

--- unext/archs.py
+++ unext/archs.py
@@ -1,5 +1,6 @@
 import torch
-from torch import nn
+# from torch import nn
+import torch.nn as nn
 import torch
 import torchvision
 from torch import nn
@@ -14,12 +15,14 @@ import matplotlib.pyplot as plt
 from utils import *
 
 import timm
-from timm.models.layers import DropPath, to_2tuple, trunc_normal_
+# from timm.models.layers import DropPath, to_2tuple, trunc_normal_
 import types
 import math
 from abc import ABCMeta, abstractmethod
 from mmcv.cnn import ConvModule
 import pdb
+from einops import repeat, rearrange
+import numpy as np
 
 
 __all__ = ['UNext', 'Unet']
@@ -573,3 +576,335 @@ class Unet(nn.Module):
             x = self.ups[idx+1](concat_skip)
         
         return self.final_conv(x)
+
+
+class TransformerEncoder(nn.Module):
+    def __init__(self, embed_dim, num_heads, num_layers, dropout, extract_layers, dim_linear_block):
+        super().__init__()
+        self.layer = nn.ModuleList()
+        self.extract_layers = extract_layers
+
+        # makes TransformerBlock device aware
+        self.block_list = nn.ModuleList()
+        for _ in range(num_layers):
+            self.block_list.append(TransformerBlock(dim=embed_dim, heads=num_heads,
+                                                    dim_linear_block=dim_linear_block, dropout=dropout, prenorm=True))
+
+    def forward(self, x):
+        extract_layers = []
+        for depth, layer_block in enumerate(self.block_list):
+            x = layer_block(x)
+            if (depth + 1) in self.extract_layers:
+                extract_layers.append(x)
+
+        return extract_layers
+
+# yellow block in Fig.1
+class Conv3DBlock(nn.Module):
+    def __init__(self, in_planes, out_planes, kernel_size=3, double=True, norm=nn.BatchNorm3d, skip=True):
+        super().__init__()
+        self.skip = skip
+        self.downsample = in_planes != out_planes
+        self.final_activation = nn.LeakyReLU(negative_slope=0.01,inplace=True) 
+        padding = (kernel_size - 1) // 2
+        if double:
+            self.conv_block = nn.Sequential(
+                nn.Conv3d(in_planes, out_planes, kernel_size=kernel_size, stride=1,
+                          padding=padding),
+                norm(out_planes),
+                nn.LeakyReLU(negative_slope=0.01,inplace=True),
+                nn.Conv3d(out_planes, out_planes, kernel_size=kernel_size, stride=1,
+                          padding=padding),
+                norm(out_planes))
+        else:
+            self.conv_block = nn.Sequential(
+                nn.Conv3d(in_planes, out_planes, kernel_size=kernel_size, stride=1,
+                          padding=padding),
+                norm(out_planes))
+
+        if self.skip and self.downsample:
+            self.conv_down = nn.Sequential(
+                nn.Conv3d(in_planes, out_planes, kernel_size=1, stride=1,
+                          padding=0),
+                norm(out_planes))
+
+    def forward(self, x):
+        y = self.conv_block(x)
+        if self.skip:
+            res = x
+            if self.downsample:
+                res = self.conv_down(res)
+            y = y + res
+        return self.final_activation(y)
+
+
+def compute_mhsa(q, k, v, scale_factor=1, mask=None):
+    # resulted shape will be: [batch, heads, tokens, tokens]
+    scaled_dot_prod = torch.einsum('... i d , ... j d -> ... i j', q, k) * scale_factor
+
+    if mask is not None:
+        assert mask.shape == scaled_dot_prod.shape[2:]
+        scaled_dot_prod = scaled_dot_prod.masked_fill(mask, -np.inf)
+
+    attention = torch.softmax(scaled_dot_prod, dim=-1)
+    # calc result per head
+    return torch.einsum('... i j , ... j d -> ... i d', attention, v)
+
+
+class MultiHeadSelfAttention(nn.Module):
+    def __init__(self, dim, heads=8, dim_head=None):
+        """
+        Implementation of multi-head attention layer of the original transformer model.
+        einsum and einops.rearrange is used whenever possible
+        Args:
+            dim: token's dimension, i.e. word embedding vector size
+            heads: the number of distinct representations to learn
+            dim_head: the dim of the head. In general dim_head<dim.
+            However, it may not necessary be (dim/heads)
+        """
+        super().__init__()
+        self.dim_head = (int(dim / heads)) if dim_head is None else dim_head
+        _dim = self.dim_head * heads
+        self.heads = heads
+        self.to_qvk = nn.Linear(dim, _dim * 3, bias=False)
+        self.W_0 = nn.Linear(_dim, dim, bias=False)
+        self.scale_factor = self.dim_head ** -0.5
+
+    def forward(self, x, mask=None):
+        assert x.dim() == 3
+        qkv = self.to_qvk(x)  # [batch, tokens, dim*3*heads ]
+
+        # decomposition to q,v,k and cast to tuple
+        # the resulted shape before casting to tuple will be: [3, batch, heads, tokens, dim_head]
+        q, k, v = tuple(rearrange(qkv, 'b t (d k h ) -> k b h t d ', k=3, h=self.heads))
+
+        out = compute_mhsa(q, k, v, mask=mask, scale_factor=self.scale_factor)
+
+        # re-compose: merge heads with dim_head
+        out = rearrange(out, "b h t d -> b t (h d)")
+        # Apply final linear transformation layer
+        return self.W_0(out)
+
+
+class TransformerBlock(nn.Module):
+    """
+    Vanilla transformer block from the original paper "Attention is all you need"
+    Detailed analysis: https://theaisummer.com/transformer/
+    """
+
+    def __init__(self, dim, heads=8, dim_head=None,
+                 dim_linear_block=1024, dropout=0.1, activation=nn.GELU,
+                 mhsa=None, prenorm=False):
+        """
+        Args:
+            dim: token's vector length
+            heads: number of heads
+            dim_head: if none dim/heads is used
+            dim_linear_block: the inner projection dim
+            dropout: probability of droppping values
+            mhsa: if provided you can change the vanilla self-attention block
+            prenorm: if the layer norm will be applied before the mhsa or after
+        """
+        super().__init__()
+        self.mhsa = mhsa if mhsa is not None else MultiHeadSelfAttention(dim=dim, heads=heads, dim_head=dim_head)
+        self.prenorm = prenorm
+        self.drop = nn.Dropout(dropout)
+        self.norm_1 = nn.LayerNorm(dim)
+        self.norm_2 = nn.LayerNorm(dim)
+
+        self.linear = nn.Sequential(
+            nn.Linear(dim, dim_linear_block),
+            activation(),  # nn.ReLU or nn.GELU
+            nn.Dropout(dropout),
+            nn.Linear(dim_linear_block, dim),
+            nn.Dropout(dropout)
+        )
+
+    def forward(self, x, mask=None):
+        if self.prenorm:
+            y = self.drop(self.mhsa(self.norm_1(x), mask)) + x
+            out = self.linear(self.norm_2(y)) + y
+        else:
+            y = self.norm_1(self.drop(self.mhsa(x, mask)) + x)
+            out = self.norm_2(self.linear(y) + y)
+        return out
+
+
+# green block in Fig.1
+class TranspConv3DBlock(nn.Module):
+    def __init__(self, in_planes, out_planes):
+        super().__init__()
+        self.block = nn.ConvTranspose3d(in_planes, out_planes, kernel_size=2, stride=2,
+                                        padding=0, output_padding=0,bias=False)
+
+    def forward(self, x):
+        y = self.block(x)
+        return y
+
+
+class BlueBlock(nn.Module):
+    def __init__(self, in_planes, out_planes, layers=1, conv_block=False):
+        """
+        blue box in Fig.1
+        Args:
+            in_planes: in channels of transpose convolution
+            out_planes: out channels of transpose convolution
+            layers: number of blue blocks, transpose convs
+            conv_block: whether to include a conv block after each transpose conv. deafaults to False
+        """
+        super().__init__()
+        self.blocks = nn.ModuleList([TranspConv3DBlock(in_planes, out_planes),
+                                            ])
+        if conv_block:
+            self.blocks.append(Conv3DBlock(out_planes, out_planes, double=False))
+
+        if int(layers)>=2:
+            for _ in range(int(layers) - 1):
+                self.blocks.append(TranspConv3DBlock(out_planes, out_planes))
+                if conv_block:
+                    self.blocks.append(Conv3DBlock(out_planes, out_planes, double=False))
+
+    def forward(self, x):
+        for blk in self.blocks:
+            x = blk(x)
+        return x
+
+
+def expand_to_batch(tensor, desired_size):
+    tile = desired_size // tensor.shape[0]
+    return repeat(tensor, 'b ... -> (b tile) ...', tile=tile)
+
+
+class AbsPositionalEncoding1D(nn.Module):
+    def __init__(self, tokens, dim):
+        super(AbsPositionalEncoding1D, self).__init__()
+        self.abs_pos_enc = nn.parameter.Parameter(torch.randn(1,tokens, dim))
+
+    def forward(self, x):
+        batch = x.size()[0]
+        return x + expand_to_batch(self.abs_pos_enc, desired_size=batch)
+
+
+class Embeddings3D(nn.Module):
+    def __init__(self, input_dim, embed_dim, cube_size, patch_size=16, dropout=0.1):
+        super().__init__()
+        self.n_patches = int((cube_size[0] * cube_size[1] * cube_size[2]) / (patch_size * patch_size * patch_size))
+        self.patch_size = patch_size
+        self.embed_dim = embed_dim
+        self.patch_embeddings = nn.Conv3d(in_channels=input_dim, out_channels=embed_dim,
+                                          kernel_size=patch_size, stride=patch_size, bias=False)
+        self.position_embeddings = AbsPositionalEncoding1D(self.n_patches, embed_dim)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        """
+        x is a 5D tensor
+        """
+        x = rearrange(self.patch_embeddings(x), 'b d x y z -> b (x y z) d')
+        embeddings = self.dropout(self.position_embeddings(x))
+        return embeddings
+
+
+# based on https://arxiv.org/abs/2103.10504
+# implementation is influenced by practical details missing in the paper that can be found
+# https://github.com/Project-MONAI/MONAI/blob/027947bf91ff0dfac94f472ed1855cd49e3feb8d/monai/networks/nets/unetr.py
+class UNETR(nn.Module):
+    def __init__(self, img_shape=(128, 128, 128), input_dim=4, output_dim=3,
+                 embed_dim=768, patch_size=16, num_heads=12, dropout=0.0,
+                 ext_layers=[3, 6, 9, 12], norm='instance',
+                 base_filters=16,
+                 dim_linear_block=3072):
+        """
+        Args:
+            img_shape: volume shape, provided as a tuple
+            input_dim: input modalities/channels
+            output_dim: number of classes
+            embed_dim: transformer embed dim.
+            patch_size: the non-overlapping patches to be created
+            num_heads: for the transformer encoder
+            dropout: percentage for dropout
+            ext_layers: transformer layers to use their output
+            version: 'light' saves some parameters in the decoding part
+            norm: batch or instance norm for the conv blocks
+        """
+        super().__init__()
+        self.num_layers = 12
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        self.embed_dim = embed_dim
+        self.img_shape = img_shape
+        self.patch_size = patch_size
+        self.num_heads = num_heads
+        self.dropout = dropout
+        self.ext_layers = ext_layers
+        self.patch_dim = [int(x / patch_size) for x in img_shape]
+
+        self.norm = nn.BatchNorm3d if norm == 'batch' else nn.InstanceNorm3d
+
+        self.embed = Embeddings3D(input_dim=input_dim, embed_dim=embed_dim,
+                                  cube_size=img_shape, patch_size=patch_size, dropout=dropout)
+
+        self.transformer = TransformerEncoder(embed_dim, num_heads,
+                                              self.num_layers, dropout, ext_layers,
+                                              dim_linear_block=dim_linear_block)
+
+        self.init_conv = Conv3DBlock(input_dim, base_filters, double=True, norm=self.norm)
+
+        # blue blocks in Fig.1
+        self.z3_blue_conv = BlueBlock(in_planes=embed_dim, out_planes=base_filters * 2, layers=3)
+
+        self.z6_blue_conv = BlueBlock(in_planes=embed_dim, out_planes=base_filters * 4, layers=2)
+
+        self.z9_blue_conv = BlueBlock(in_planes=embed_dim, out_planes=base_filters * 8, layers=1)
+
+        # Green blocks in Fig.1
+        self.z12_deconv = TranspConv3DBlock(embed_dim, base_filters * 8)
+
+        self.z9_deconv = TranspConv3DBlock(base_filters * 8, base_filters * 4)
+        self.z6_deconv = TranspConv3DBlock(base_filters * 4, base_filters * 2)
+        self.z3_deconv = TranspConv3DBlock(base_filters * 2, base_filters)
+
+        # Yellow blocks in Fig.1
+        self.z9_conv = Conv3DBlock(base_filters * 8 * 2, base_filters * 8, double=True, norm=self.norm)
+        self.z6_conv = Conv3DBlock(base_filters * 4 * 2, base_filters * 4, double=True, norm=self.norm)
+        self.z3_conv = Conv3DBlock(base_filters * 2 * 2, base_filters * 2, double=True, norm=self.norm)
+        # out convolutions
+        self.out_conv = nn.Sequential(
+            # last yellow conv block
+            Conv3DBlock(base_filters * 2, base_filters, double=True, norm=self.norm),
+            # grey block, final classification layer
+            nn.Conv3d(base_filters, output_dim, kernel_size=1, stride=1))
+
+    def forward(self, x):
+        transf_input = self.embed(x)
+        z3, z6, z9, z12 = map(lambda t: rearrange(t, 'b (x y z) d -> b d x y z',
+                                                  x=self.patch_dim[0], y=self.patch_dim[1], z=self.patch_dim[2]),
+                              self.transformer(transf_input))
+
+        # Blue convs
+        z0 = self.init_conv(x)
+        z3 = self.z3_blue_conv(z3)
+        z6 = self.z6_blue_conv(z6)
+        z9 = self.z9_blue_conv(z9)
+
+        # Green block for z12
+        z12 = self.z12_deconv(z12)
+        # Concat + yellow conv
+        y = torch.cat([z12, z9], dim=1)
+        y = self.z9_conv(y)
+
+        # Green block for z6
+        y = self.z9_deconv(y)
+        # Concat + yellow conv
+        y = torch.cat([y, z6], dim=1)
+        y = self.z6_conv(y)
+
+        # Green block for z3
+        y = self.z6_deconv(y)
+        # Concat + yellow conv
+        y = torch.cat([y, z3], dim=1)
+        y = self.z3_conv(y)
+
+        y = self.z3_deconv(y)
+        y = torch.cat([y, z0], dim=1)
+        return self.out_conv(y)
