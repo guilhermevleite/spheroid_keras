9cee4efb20b9d84953a75f9130cfee5471b41808
Guilherme Leite <guilherme.vieira.leite@gmail.com>
2022-11-16 13:02:07 -0300

    unet pp architecture ready for testing

--- unext/train.sh
+++ unext/train.sh
@@ -23,7 +23,9 @@ idx=0
 
 #python3 train.py --dataset isic_2018 --arch Unet --name ${idx}_unet_isic_200ep --img_ext .jpg --mask_ext .png --lr 0.0001 --epochs 200 --input_w 256 --input_h 256 --b 6
 
-python3 train.py --dataset busi --arch Unet --name ${idx}_unet_busi_2ep --img_ext .png --mask_ext .png --lr 0.0001 --epochs 2 --input_w 256 --input_h 256 --b 6
+#python3 train.py --dataset busi --arch Unet --name ${idx}_unet_busi_2ep --img_ext .png --mask_ext .png --lr 0.0001 --epochs 2 --input_w 256 --input_h 256 --b 6
+#
+python3 train.py --dataset busi --arch UnetPP --name ${idx}_unetpp_busi_2ep --img_ext .png --mask_ext .png --lr 0.0001 --epochs 2 --input_w 256 --input_h 256 --b 6
 
 #python3 train.py --dataset isic_2018 --arch UNext --name ${idx}_unext_isic_200ep --img_ext .jpg --mask_ext .png --lr 0.0001 --epochs 200 --input_w 256 --input_h 256 --b 6
 
