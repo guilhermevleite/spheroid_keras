6a6f387bf412beb98037ab16e367aa1ec1bf5413
Guilherme Leite <guilherme.vieira.leite@gmail.com>
2022-12-01 23:15:01 -0300

    trying to add transUNET

--- bash.sh
+++ bash.sh
@@ -1,1 +1,1 @@
-docker run -it --name=bash --gpus all --rm -v /home/leite/workspace:/workspace -w /workspace/spheroid_keras/unext -u $(id -u):$(id -g) phd_torch bash
+docker run -it --name=bash --gpus all --rm -v /home/leite/workspace:/workspace -w /workspace/spheroid_keras/unext -u $(id -u):$(id -g) phd_torch:1.2 bash
